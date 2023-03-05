1102a1864b7ddf933918455fd50bbc11c9a79e97
Şəbnəm <mmmdovasbnm47@gmail.com>
2023-03-06 01:30:56 +0400

    first commit

---
+++ Task 03.05/Task 03.05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task 3
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2104;
+            int teklik = 4, onluq = 0, yuzluk = 1, minlik = 2;
+            Console.WriteLine(teklik + onluq + yuzluk + minlik);
+
+        }
+    }
+}
+ 
